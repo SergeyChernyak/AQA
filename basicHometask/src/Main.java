2689276df13da52c03e68c4aee2e5cd51bbf927e
Serj <Striker-Life@mail.ru>
2018-12-18 23:58:12 +0200

    Completed tasks including loops

--- basicHometask/src/Main.java
+++ basicHometask/src/Main.java
@@ -2,13 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
         Task task = new Task();
-
-        //task.hello();
-        //System.out.println("Average array = " + task.averageMas());
-        //System.out.println("Factorial = " + task.factorialValue());
-        //task.printPrice();
-        //task.checkValue();
-        //task.arrayToStr();
+        task.hello();
+        System.out.println("Average array = " + task.averageMas());
+        System.out.println("Factorial = " + task.factorialValue());
+        task.printPrice();
+        task.checkValue();
+        task.arrayToStrAndStrToArray();
         task.addHelloToArray();
+        task.arrayToBool();
+        task.whereToGo();
+        task.cycleBefore();
+        task.cycleAfter();
+        task.printValueOfArray();
     }
 }
--- basicHometask/src/Task.java
+++ basicHometask/src/Task.java
@@ -1,10 +1,12 @@
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Task {
 
     private Scanner scanner = new Scanner(System.in);
+    private Random random = new Random();
 
     protected void hello () {
         System.out.println("Hello World");
@@ -14,7 +16,6 @@ public class Task {
         System.out.print("Enter length array = ");
         int n = scanner.nextInt();
         int[] array = new int[n];
-        Random random = new Random();
         double averageValue = 0;
 
         for (int i=0; i < array.length; i++) {
@@ -67,13 +68,17 @@ public class Task {
             System.out.println("The number " + n + " is not event!");
     }
 
-    protected void arrayToStr () {
+    protected void arrayToStrAndStrToArray() {
         int[] array = {1,2,3,4,5};
-        String arrayStr = "";
+        String arrayStr;
         char[] newArray;
         arrayStr = Arrays.toString(array);
         System.out.println("Array to String = " + arrayStr);
-        newArray = arrayStr.toCharArray();
+        String str1 = arrayStr.replace("[", "");
+        String str2 = str1.replace("]", "");
+        String str3 = str2.replace(",", "");
+        String str4 = str3.replace(" ", "");
+        newArray = str4.toCharArray();
         for (int i = 0; i < newArray.length; i++) {
             System.out.println("array[" + i + "] = " + newArray[i]);
         }
@@ -85,6 +90,71 @@ public class Task {
             array[i] = array[i] + "hello";
         }
         System.out.println("Array with hello = " + Arrays.toString(array));
+    }
+
+    protected void arrayToBool () {
+        int[] array = {1,0,3,4,0};
+        System.out.println("Input int array = " + Arrays.toString(array));
+        boolean[] arrayBool = new boolean[array.length];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 0) {
+                    arrayBool[i] = true;
+                } else
+                    arrayBool[i] = false;
+            }
+        System.out.println("Int array to Boolean array = " + Arrays.toString(arrayBool));
+    }
+
+    protected void whereToGo () {
+        System.out.print("Enter now time = ");
+        double time = scanner.nextDouble();
+        System.out.print("Enter money = ");
+        double money = scanner.nextDouble();
+
+        if (time >= 8 & time <= 12 & money >= 10) {
+            System.out.println("Go to shop");
+        } else if (time > 12 & time < 19 & money >= 50) {
+            System.out.println("Go to cafe");
+        } else if (time <= 19 & time > 12 & money < 50) {
+            System.out.println("Go to neighbour");
+        } else if (time > 19 & time <= 22){
+            System.out.println("Watch TV");
+        } else if (time > 22  & time <=24) {
+            System.out.println("Go to bed");
+        } else
+            System.out.println("Wrong time");
+    }
+
+    protected void cycleBefore() {
+        System.out.print("Enter value = ");
+        int n = scanner.nextInt();
+        while (n > 10) {
+            System.out.println(n);
+            n--;
+        }
+    }
+
+    protected void cycleAfter () {
+        System.out.print("Enter value = ");
+        int n = scanner.nextInt();
+
+        do {
+            System.out.println(n);
+            n--;
+        } while (n > 10);
+    }
+
+    protected void printValueOfArray () {
+        int[] array = new int[10];
+        for (int i = 0; i<array.length; i++) {
+            array[i] = random.nextInt(20);
+        }
+        System.out.println(Arrays.toString(array));
+
+        for (int x: array) {
+            if (x == 5)
+                System.out.println("Value = " + x);
+        }
     }
 
 }
